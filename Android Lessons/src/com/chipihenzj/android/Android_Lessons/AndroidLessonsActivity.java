9e0706438ae6cc3099fe33ccb6b4ba92779fb282
ChipiHenzj <opriadko@zeuselectronics.eu>
2015-01-29 15:22:27 +0200

    Lesson 15. Context Menu. XML.

--- Android Lessons/src/com/chipihenzj/android/Android_Lessons/AndroidLessonsActivity.java
+++ Android Lessons/src/com/chipihenzj/android/Android_Lessons/AndroidLessonsActivity.java
@@ -4,26 +4,24 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 
 /**
- *   Lesson 15. Context Menu
+ *   Lesson 15. Context Menu. XML.
  */
 
 public class AndroidLessonsActivity extends Activity {
-
     TextView tvColor, tvSize;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
 
         tvColor = (TextView)findViewById(R.id.tvColor);
         tvSize = (TextView)findViewById(R.id.tvSize);
@@ -32,70 +30,60 @@ public class AndroidLessonsActivity extends Activity {
         registerForContextMenu(tvSize);
     }
 
-    final int MENU_COLOR_RED = 1;
-    final int MENU_COLOR_GREEN = 2;
-    final int MENU_COLOR_BLUE = 3;
-
-    final int MENU_SIZE_22 = 4;
-    final int MENU_SIZE_26 = 5;
-    final int MENU_SIZE_30 = 6;
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v,
-     ContextMenu.ContextMenuInfo menuInfo){
-        switch (v.getId()){
-     case R.id.tvColor:
-         menu.add(0, MENU_COLOR_RED, 0, "Red" );
-         menu.add(0, MENU_COLOR_GREEN, 0, "Green");
-         menu.add(0, MENU_COLOR_BLUE, 0, "Blue");
+    public void onCreateContextMenu (ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        switch (v.getId()) {
+    case R.id.tvColor:
+         getMenuInflater().inflate(R.menu.menu, menu);
+         menu.setGroupVisible(R.id.group2, false);
          break;
-     case R.id.tvSize:
-         menu.add(0, MENU_SIZE_22, 0, "22"  );
-         menu.add(0, MENU_SIZE_26, 0, "26" );
-         menu.add(0, MENU_SIZE_30, 0, "30" );
+    case R.id.tvSize:
+         getMenuInflater().inflate(R.menu.menu, menu);
+         menu.setGroupVisible(R.id.group1, false);
          break;
         }
-     }
+    }
+
     @Override
-    public boolean onContextItemSelected(MenuItem item){
+    public boolean onContextItemSelected(MenuItem item) {
 
-        switch(item.getItemId()){
-
-    case MENU_COLOR_RED:
-        tvColor.setTextColor(Color.RED);
-        tvColor.setText("Text color = red");
-        break;
-
-    case MENU_COLOR_GREEN:
+        switch (item.getItemId()) {
+    case R.id.MENU_COLOR_RED:
+         tvColor.setTextColor(Color.RED);
+         tvColor.setText("Text color = red");
+         break;
+    case R.id.MENU_COLOR_GREEN:
          tvColor.setTextColor(Color.GREEN);
          tvColor.setText("Text color = green");
          break;
-
-    case MENU_COLOR_BLUE:
-        tvColor.setTextColor(Color.BLUE);
-        tvColor.setText("Text color = blue");
-        break;
-
-    case MENU_SIZE_22:
-        tvSize.setTextSize(22);
-        tvSize.setText("Text size = 22");
-        break;
-
-    case MENU_SIZE_26:
-        tvSize.setTextSize(26);
-        tvSize.setText("Text size = 26");
-        break;
-
-    case MENU_SIZE_30:
-        tvSize.setTextSize(30);
-        tvSize.setText("Text size = 30");
-        break;
-        }
+    case R.id.MENU_COLOR_BLUE:
+         tvColor.setTextColor(Color.BLUE);
+         tvColor.setText("Text color = blue");
+         break;
+    case R.id.MENU_SIZE_22:
+         tvSize.setTextSize(22);
+         tvSize.setText("Text size = 22");
+         break;
+    case R.id.MENU_SIZE_26:
+         tvSize.setTextSize(26);
+         tvSize.setText("Text size = 26");
+         break;
+    case R.id.MENU_SIZE_30:
+         tvSize.setTextSize(30);
+         tvSize.setText("Text size = 30");
+         break;
+         }
         return super.onContextItemSelected(item);
     }
 
-
 }
+
+
+
+
+
+
+
+
 
 
 
