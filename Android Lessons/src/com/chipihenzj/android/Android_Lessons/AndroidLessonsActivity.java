fc6d18ca733bba3440feeb307a8777e80a49eebe
ChipiHenzj <opriadko@zeuselectronics.eu>
2015-02-04 16:43:28 +0200

    Lesson 16. Software creation screen. LayoutParams

--- Android Lessons/src/com/chipihenzj/android/Android_Lessons/AndroidLessonsActivity.java
+++ Android Lessons/src/com/chipihenzj/android/Android_Lessons/AndroidLessonsActivity.java
@@ -1,80 +1,61 @@
 package com.chipihenzj.android.Android_Lessons;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.*;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-
-
 /**
- *   Lesson 15. Context Menu. XML.
+ *   Lesson 16. Software creation screen. LayoutParams
  */
 
 public class AndroidLessonsActivity extends Activity {
-    TextView tvColor, tvSize;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        LinearLayout linLayout = new LinearLayout(this);
+
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+
+        ViewGroup.LayoutParams linLayoutParam = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+
+        setContentView(linLayout, linLayoutParam);
+
+        ViewGroup.LayoutParams lpView = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        TextView tv = new TextView(this);
+        tv.setText("TextView");
+        tv.setLayoutParams(lpView);
+        linLayout.addView(tv);
+
+        Button btn = new Button(this);
+        btn.setText("Button");
+        linLayout.addView(btn, lpView);
 
 
-        tvColor = (TextView)findViewById(R.id.tvColor);
-        tvSize = (TextView)findViewById(R.id.tvSize);
+        LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        leftMarginParams.leftMargin = 50;
 
-        registerForContextMenu(tvColor);
-        registerForContextMenu(tvSize);
+        Button btn1 = new Button(this);
+        btn1.setText("Button1");
+        linLayout.addView(btn1, leftMarginParams);
+
+
+        LinearLayout.LayoutParams rightGravityParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        Button btn2 = new Button(this);
+        btn2.setText("Button2");
+        linLayout.addView(btn2, rightGravityParams);
     }
-
-    public void onCreateContextMenu (ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
-        switch (v.getId()) {
-    case R.id.tvColor:
-         getMenuInflater().inflate(R.menu.menu, menu);
-         menu.setGroupVisible(R.id.group2, false);
-         break;
-    case R.id.tvSize:
-         getMenuInflater().inflate(R.menu.menu, menu);
-         menu.setGroupVisible(R.id.group1, false);
-         break;
-        }
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-    case R.id.MENU_COLOR_RED:
-         tvColor.setTextColor(Color.RED);
-         tvColor.setText("Text color = red");
-         break;
-    case R.id.MENU_COLOR_GREEN:
-         tvColor.setTextColor(Color.GREEN);
-         tvColor.setText("Text color = green");
-         break;
-    case R.id.MENU_COLOR_BLUE:
-         tvColor.setTextColor(Color.BLUE);
-         tvColor.setText("Text color = blue");
-         break;
-    case R.id.MENU_SIZE_22:
-         tvSize.setTextSize(22);
-         tvSize.setText("Text size = 22");
-         break;
-    case R.id.MENU_SIZE_26:
-         tvSize.setTextSize(26);
-         tvSize.setText("Text size = 26");
-         break;
-    case R.id.MENU_SIZE_30:
-         tvSize.setTextSize(30);
-         tvSize.setText("Text size = 30");
-         break;
-         }
-        return super.onContextItemSelected(item);
-    }
-
 }
 
 
