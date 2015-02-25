513bdc1f0e27a3b5b140e2e3f5aa5b30cc87c960
ChipiHenzj <opriadko@zeuselectronics.eu>
2015-02-25 18:13:35 +0200

    Lesson 26. Intent Filter - Practice

--- Android Lessons/src/com/chipihenzj/android/Android_Lessons/AndroidLessonsActivity.java
+++ Android Lessons/src/com/chipihenzj/android/Android_Lessons/AndroidLessonsActivity.java
@@ -9,69 +9,42 @@ import android.widget.Button;
 
 
 /**
- *   Lesson 25. Task. What is it and how is formed
+ *   Lesson 26. Intent Filter - Practice
  */
 
-public class AndroidLessonsActivity extends Activity implements View.OnClickListener {
+public class AndroidLessonsActivity extends Activity implements View.OnClickListener{
 
-
-    final String TAG = "States";
-
-    Button btnActTwo;
+    Button btnTime;
+    Button btnDate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        btnActTwo = (Button)findViewById(R.id.btnActTwo);
-        btnActTwo.setOnClickListener(this);
+        btnTime  = (Button)findViewById(R.id.btnTime);
+        btnTime.setOnClickListener(this);
 
-        Log.d(TAG,"MainActivity: onCreate()");
+        btnDate = (Button)findViewById(R.id.btnDate);
+        btnDate.setOnClickListener(this);
     }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "MainActivity: onRestart()");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "MainActivity: onStart()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "MainActivity: onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "MainActivity: onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "MainActivity: onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "MainActivity: onDestroy()");
-    }
-
 
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, ActivityTwo.class);
+        Intent intent;
+
+        switch(v.getId()){
+    case R.id.btnTime:
+        intent = new Intent("startandroid.intent.action.showtime");
         startActivity(intent);
+        break;
+    case R.id.btnDate:
+        intent = new Intent("startandroid.intent.action.showdate");
+        startActivity(intent);
+        break;
+
+        }
 
     }
 }
