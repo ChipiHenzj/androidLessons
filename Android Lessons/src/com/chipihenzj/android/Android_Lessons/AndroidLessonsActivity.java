555de532f9d377ba48be0318d577e2683bac6d7d
ChipiHenzj <opriadko@zeuselectronics.eu>
2015-05-08 14:42:34 +0300

    Lesson 41. Use LayoutInflater to create a list.

--- Android Lessons/src/com/chipihenzj/android/Android_Lessons/AndroidLessonsActivity.java
+++ Android Lessons/src/com/chipihenzj/android/Android_Lessons/AndroidLessonsActivity.java
@@ -1,88 +1,67 @@
 package com.chipihenzj.android.Android_Lessons;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
-/** Lesson 40. LayoutInflater. Learning to use.
+/** Lesson 41. Use LayoutInflater to create a list.
  *
  */
 
 public class AndroidLessonsActivity extends Activity {
 
-    final String LOG_TAG = "myLogs";
+    String[] name = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь" };
 
+    String[] position = { "Программер", "Бухгалтер", "Программер",
+            "Программер", "Бухгалтер", "Директор", "Программер", "Охранник" };
+
+    int salary[] = { 13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000 };
+
+    int[] colors = new int[2];
 
     @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.main);
-//
-//        LayoutInflater ltInflater = getLayoutInflater();
-//        View view = ltInflater.inflate(R.layout.text, null, false);
-//        ViewGroup.LayoutParams lp = view.getLayoutParams();
-//
-//        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
-//        linLayout.addView(view);
-//
-//        Log.d(LOG_TAG, "Class of view: " + view.getClass().toString());
-//        Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp == null));
-//        Log.d(LOG_TAG, "Text of view: " + ((TextView) view).getText());
-//
-//    }
-
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.main);
-//
-//        LayoutInflater ltInflater = getLayoutInflater();
-//
-//        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
-//        View view1 = ltInflater.inflate(R.layout.text, linLayout, false);
-//        ViewGroup.LayoutParams lp1 = view1.getLayoutParams();
-//
-//        Log.d(LOG_TAG, "Class of view1: " + view1.getClass().toString());
-//        Log.d(LOG_TAG, "Class of layoutParams of view1: " + lp1.getClass().toString());
-//        Log.d(LOG_TAG, "Text of view1: " + ((TextView) view1).getText());
-//
-//        RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
-//        View view2 = ltInflater.inflate(R.layout.text, relLayout, false);
-//        ViewGroup.LayoutParams lp2 = view2.getLayoutParams();
-//
-//        Log.d(LOG_TAG, "Class of view2: " + view2.getClass().toString());
-//        Log.d(LOG_TAG, "Class of layoutParams of view2: " + lp2.getClass().toString());
-//        Log.d(LOG_TAG, "Text of view2: " + ((TextView) view2).getText());
-//    }
-
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        colors[0] = Color.parseColor("#559966CC");
+        colors[1] = Color.parseColor("#55336699");
+
+        LinearLayout linLayout = (LinearLayout)findViewById(R.id.linLayout);
+
         LayoutInflater ltInflater = getLayoutInflater();
 
-        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
-        View view1 = ltInflater.inflate(R.layout.text, linLayout, true);
-        ViewGroup.LayoutParams lp1 = view1.getLayoutParams();
+        for (int i = 0; i < name.length; i++) {
 
-        Log.d(LOG_TAG, "Class of view1: " + view1.getClass().toString());
-        Log.d(LOG_TAG, "Class of layoutParams of view1: " + lp1.getClass().toString());
+            Log.d("myLogs", "i = " + i);
 
-        RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
-        View view2 = ltInflater.inflate(R.layout.text, relLayout, true);
-        ViewGroup.LayoutParams lp2 = view2.getLayoutParams();
+            View item = ltInflater.inflate(R.layout.item, linLayout, false);
 
-        Log.d(LOG_TAG, "Class of view2: " + view2.getClass().toString());
-        Log.d(LOG_TAG, "Class of layoutParams of view2: " + lp2.getClass().toString());
+            TextView tvName = (TextView)item.findViewById(R.id.tvName);
+            tvName.setText(name[i]);
+
+            TextView tvPosition = (TextView)item.findViewById(R.id.tvPosition);
+            tvPosition.setText("Должность: " + position[i]);
+
+            TextView tvSalary = (TextView)item.findViewById(R.id.tvSalary);
+            tvSalary.setText("Оклад: " + String.valueOf(salary[i]));
+
+            item.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+            item.setBackgroundColor(colors[i % 2]);
+
+            linLayout.addView(item);
+        }
     }
 }
+
 
 
 
