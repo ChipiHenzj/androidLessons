3724fff41848a1507f6e8acb55d61c2a6c227cca
Tetiana Priadko <tetianahenzora@gmail.com>
2016-04-06 17:43:34 +0300

    People 06/04

---
+++ People/app/src/main/java/com/example/tetianapriadko/people/FragListStudent.java
@@ -0,0 +1,44 @@
+package com.example.tetianapriadko.people;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragListStudent extends Fragment {
+
+    private static final String TITLE = "List of Students";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+       View view =  inflater.inflate(R.layout.frag_list_student, container, false);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        toolbar.setTitle(TITLE);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+        android.support.v7.app.ActionBarDrawerToggle toggle = new
+                android.support.v7.app.ActionBarDrawerToggle(
+                 getActivity(),
+                drawer,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        return view;
+    }
+
+
+}
---
+++ People/app/src/main/java/com/example/tetianapriadko/people/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.tetianapriadko.people;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_settings);
+
+    }
+
+}
