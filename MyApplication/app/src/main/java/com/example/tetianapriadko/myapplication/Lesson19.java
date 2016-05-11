9bb76a09681d14d1faadb95cb7f4c22731ca95ce
Tetiana Priadko <tetianahenzora@gmail.com>
2016-05-11 17:51:01 +0300

    11/5

--- MyApplication/app/src/main/java/com/example/tetianapriadko/myapplication/Lesson19.java
+++ MyApplication/app/src/main/java/com/example/tetianapriadko/myapplication/Lesson19.java
@@ -1,6 +1,5 @@
 package com.example.tetianapriadko.myapplication;
 
-import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -23,31 +22,27 @@ public class Lesson19 extends AppCompatActivity  implements View.OnClickListener
     Button btnDiv;
 
     TextView tvResult;
-
     String symbol = "";
 
-    final int MENU_RESET_ID = 1;
-    final int MENU_QUIT_ID = 2;
-
+    public static final int MENU_RESET_ID = 1;
+    public static final int MENU_QUIT_ID = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson19);
 
-        etNum1 = (EditText)findViewById(R.id.etNum1);
-        etNum2 = (EditText)findViewById(R.id.etNum2);
+        etNum1 = ((EditText) findViewById(R.id.etNum1));
+        etNum2 = ((EditText) findViewById(R.id.etNum2));
 
-        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnAdd = ((Button) findViewById(R.id.btnAdd));
+        btnSub = ((Button) findViewById(R.id.btnSub));
+        btnMult = ((Button) findViewById(R.id.btnMult));
+        btnDiv = ((Button) findViewById(R.id.btnDiv));
+
         btnAdd.setOnClickListener(this);
-
-        btnSub = (Button)findViewById(R.id.btnSub);
         btnSub.setOnClickListener(this);
-
-        btnMult = (Button)findViewById(R.id.btnMult);
         btnMult.setOnClickListener(this);
-
-        btnDiv = (Button)findViewById(R.id.btnDiv);
         btnDiv.setOnClickListener(this);
 
         tvResult = (TextView)findViewById(R.id.tvResult);
@@ -61,75 +56,73 @@ public class Lesson19 extends AppCompatActivity  implements View.OnClickListener
         float result = 0;
 
         if (TextUtils.isEmpty(etNum1.getText().toString())
-                || TextUtils.isEmpty(etNum2.getText().toString())) {
-            return;
+        || TextUtils.isEmpty(etNum2.getText().toString())) {
+        return;
         }
 
         num1 = Float.parseFloat(etNum1.getText().toString());
         num2 = Float.parseFloat(etNum2.getText().toString());
 
+
         switch(v.getId()){
             case R.id.btnAdd:
-                result = num1 + num2;
                 symbol = "+";
+                result = num1 + num2;
                 tvResult.setText(num1 + " " + symbol + " " + num2 + " = " + result);
                 break;
 
             case R.id.btnSub:
-                result = num1 - num2;
                 symbol = "-";
-                tvResult.setText(num1 + " " + symbol + " " + num2 + " = " + result);
+                result = num1 - num2;
+                tvResult.setText(num1 + " " + symbol+ " " + num2 + " = " + result);
                 break;
 
             case R.id.btnMult:
-                result = num1 * num2;
                 symbol = "*";
+                result = num1 * num2;
                 tvResult.setText(num1 + " " + symbol + " " + num2 + " = " + result);
                 break;
 
             case R.id.btnDiv:
-                if(num2 == 0){
+                if(num2 == 0 ){
                     Toast.makeText(this, "Cannot divide on zero", Toast.LENGTH_SHORT).show();
                     etNum2.setText("");
                     tvResult.setText("");
-                    return;
                 } else {
-                    result = num1 / num2;
                     symbol = "/";
+                    result = num1 / num2;
                     tvResult.setText(num1 + " " + symbol + " " + num2 + " = " + result);
-
                 }
                 break;
             default:
                 break;
+
         }
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         menu.add(0, MENU_RESET_ID, 0, "Reset");
         menu.add(0, MENU_QUIT_ID, 0, "Quit");
-
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case  MENU_RESET_ID:
-                etNum1.setText("");
-                etNum2.setText("");
-                tvResult.setText("");
-                Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
-                break;
-            case MENU_QUIT_ID:
-                finish();
-                Toast.makeText(this, "Quit", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
+    switch (item.getItemId()){
+        case MENU_QUIT_ID:
+            Toast.makeText(this, "Quit", Toast.LENGTH_SHORT).show();
+            finish();
+            break;
+        case MENU_RESET_ID:
+            etNum1.setText("");
+            etNum2.setText("");
+            tvResult.setText("");
+            Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
+            break;
+        default:
+            break;
+    }
 
 
         return super.onOptionsItemSelected(item);
--- MyApplication/app/src/main/java/com/example/tetianapriadko/myapplication/Lesson28_1.java
+++ MyApplication/app/src/main/java/com/example/tetianapriadko/myapplication/Lesson28_1.java
@@ -13,7 +13,6 @@ public class Lesson28_1 extends AppCompatActivity implements View.OnClickListene
     EditText etLName;
     Button btnSubmit;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
